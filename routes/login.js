8c336ad121a3a88561c96f1116e6cbaf2e91d448
Winmay <mei-tang@qq.com>
2018-04-11 15:49:15 +0800

    '给密码加盐'

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var express = require('express'),
     router = express.Router(),
     User = require('../models/user.js'),
     crypto = require('crypto'),
+    bcrypt = require('bcrypt-nodejs') ,
     TITLE_LOGIN = '登录';
 
 router.get('/', function(req, res) {
@@ -23,9 +24,36 @@ router.post('/', function(req, res) {
              return;
         }
 
-         userPwd = md5.update(userPwd).digest('hex');
-         console.log(results);
-         console.log('userName:'+userName+'\n'+'userPwd:'+userPwd);
+        userPwd = md5.update(userPwd).digest('hex');
+        //检查数据库里的加盐密码和用户输入的密码是否匹配,user.password为数据库里存储的密码
+        bcrypt.compare(userPwd, results[0].userPass,function(err,res){
+           var pwdMatchFlag = res;
+           tryLogin(pwdMatchFlag);
+           console.log(pwdMatchFlag);
+        })
+        // 尝试登录
+        function tryLogin(pwdMatchFlag){
+            if(pwdMatchFlag){
+                if(isRem)
+                {
+                    res.cookie('islogin', userName, { maxAge: 60000 });                 
+                }
+
+                res.locals.username = userName;
+                req.session.username = res.locals.username;  
+                console.log(req.session.username);                        
+                res.redirect('/');
+                return;   //匹配成功跳转到主页
+            }else{
+                res.locals.error = '用户名或密码有误';
+                res.render('login',{title:TITLE_LOGIN});
+                console.log(1);
+                return;  //匹配失败返回之前的页面
+            }
+        }
+         // console.log(results);
+         // console.log('userName:'+userName+'\n'+'userPwd:'+userPwd);
+         /*
          if(results[0].userName != userName || results[0].userPass != userPwd)
          {
              res.locals.error = '用户名或密码有误';
@@ -45,7 +73,8 @@ router.post('/', function(req, res) {
              console.log(req.session.username);                        
              res.redirect('/');
              return;
-         }     
+         } 
+         */    
     });              
 });
 
--- routes/reg.js
+++ routes/reg.js
@@ -2,7 +2,11 @@ var express = require('express'),
     router = express.Router(),
     User = require('../models/user.js'),
     crypto = require('crypto'),
+    bcrypt = require('bcrypt-nodejs') ,
+    SALT_WORK_FACTOR = 10,
     TITLE_REG = '注册';
+
+
 
 router.get('/', function(req, res) {
   res.render('reg',{title:TITLE_REG});
@@ -11,48 +15,68 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   var userName = req.body['txtUserName'],
       userPwd = req.body['txtUserPwd'],
-      userRePwd = req.body['txtUserRePwd'],      
+      userRePwd = req.body['txtUserRePwd'],
+      saltUserPwd = '',      
       md5 = crypto.createHash('md5');
  
-      userPwd = md5.update(userPwd).digest('hex');
+  var oldUserPwd = md5.update(userPwd).digest('hex');
+  /*
+   * 数据库中保存hash密码 以及对应的加密salt
+   * 10位盐
+   */
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt){ 
+      // if (err) {
+      //     return next(err)
+      // }
+      console.log('salt:'+ salt)
+      bcrypt.hash(oldUserPwd,salt ,null,function(err,hash){
+        // if(err) return next(err)
+          console.log('hash:'+hash)
+          saltUserPwd = hash;
+          storeUInfo();
+      })
+  })
 
-  var newUser = new User({
-      username: userName,
-      userpass: userPwd
-  });
+  function storeUInfo(){
+    console.log('userName：'+userName+'\n'+'saltUserPwd：'+saltUserPwd);
+    var newUser = new User({
+        username: userName,
+        userpass: saltUserPwd
+    });
 
-  //检查用户名是否已经存在
-  User.getUserNumByName(newUser.username, function (err, results) {        
-             
-      if (results != null && results[0]['num'] > 0) {
-          err = '用户名已存在';
-      }
+    //检查用户名是否已经存在
+    User.getUserNumByName(newUser.username, function (err, results) {        
+               
+        if (results != null && results[0]['num'] > 0) {
+            err = '用户名已存在';
+        }
 
-      if (err) {
-          res.locals.error = err;
-          res.render('reg', { title: TITLE_REG });
-          return;
-      }
+        if (err) {
+            res.locals.error = err;
+            res.render('reg', { title: TITLE_REG });
+            return;
+        }
 
-      newUser.save(function (err,result) {
-          if (err) {
-              res.locals.error = err;
-              res.render('reg', { title: TITLE_REG }); 
-              return;            
-          }        
+        newUser.save(function (err,result) {
+            if (err) {
+                res.locals.error = err;
+                res.render('reg', { title: TITLE_REG }); 
+                return;            
+            }        
 
-          if(result.insertId > 0)
-          {
-              res.locals.success = '注册成功,请点击   <a class="btn btn-link" href="/login" role="button"> 登录 </a>' ;
-          }
-          else
-          {
-              res.locals.error = err;
-          }
-         
-          res.render('reg', { title: TITLE_REG });
-          });    
-    });          
+            if(result.insertId > 0)
+            {
+                res.locals.success = '注册成功,请点击   <a class="btn btn-link" href="/login" role="button"> 登录 </a>' ;
+            }
+            else
+            {
+                res.locals.error = err;
+            }
+           
+            res.render('reg', { title: TITLE_REG });
+            });    
+      });
+  }
 });
 
 module.exports = router;
